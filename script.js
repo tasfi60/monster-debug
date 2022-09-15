c68c250ba45cd606bfdc4dbd1b5440720d526384
tasfi60 <tasfiagirl@gmail.com>
2022-09-15 19:31:58 +0600

    updated

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = Math.floor((finishTime - startTime) / 1000);
+  const timeTaken = Math.trunc((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -140,7 +140,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = Math.floor((currentTime - startTime) / 1000);
+  const timeSpent = Math.trunc((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
